0a8dc1efd9eee68d614559b0dd5a8b8e82612f9a
TomoyaWashio <148823993+TomoyaWashio@users.noreply.github.com>
2024-11-21 14:25:37 +0900

    HeaderTemplateScript追加

---
+++ wwwroot/script.js
@@ -0,0 +1,7 @@
+﻿igRegisterScript("JobTitleTemplate", (context) => {
+    const column = context.column;
+    const span = document.createElement("span");
+    span.textContent = column._header;
+    span.title = `HeaderTemplateScript の title 属性より\nツールチップの内容を変更します`;
+    return span;
+}, false);
